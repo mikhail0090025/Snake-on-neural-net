05efbca810a7c32040ae97c2a9076ceeef8a1736
mikhail0090025 <52649381+mikhail0090025@users.noreply.github.com>
2024-02-20 22:51:35 +0100

    Snake works

--- Game.js
+++ Game.js
@@ -1,4 +1,5 @@
 var canv = document.getElementById("game_canv");
+var CellSize = 18;
 var Point = /** @class */ (function () {
     function Point(x, y) {
         this.X = x;
@@ -7,12 +8,69 @@ var Point = /** @class */ (function () {
     Point.RandomPoint = function (max) {
         return new Point(Math.round(Math.random() * max), Math.round(Math.random() * max));
     };
+    Point.prototype.Compare = function (p) {
+        return p.X == this.X && p.Y == this.Y;
+    };
     return Point;
 }());
 var Snake = /** @class */ (function () {
     function Snake(StartPoint) {
+        this.direction = 0;
         this.Points = new Array(StartPoint);
     }
+    Snake.prototype.Head = function () {
+        return this.Points[0];
+    };
+    Snake.prototype.Tall = function () {
+        return this.Points[this.Points.length - 1];
+    };
+    Snake.prototype.NewPoint = function () {
+        var tall = this.Tall();
+        /*
+        if(this.Points.length < 2){
+            this.Points.push(new Point(tall.X, tall.Y + 1));
+            return;
+        }
+        if(this.Points[this.Points.length - 2].X == tall.X && this.Points[this.Points.length - 2].Y == tall.Y + 1){
+            this.Points.push(new Point(tall.X, tall.Y - 1));
+        }
+        else if(this.Points[this.Points.length - 2].X == tall.X + 1 && this.Points[this.Points.length - 2].Y == tall.Y){
+            this.Points.push(new Point(tall.X - 1, tall.Y));
+        }
+        else if(this.Points[this.Points.length - 2].X == tall.X - 1 && this.Points[this.Points.length - 2].Y == tall.Y){
+            this.Points.push(new Point(tall.X + 1, tall.Y));
+        }
+        else this.Points.push(new Point(tall.X, tall.Y + 1));
+        */
+        this.Points.push(new Point(tall.X, tall.Y + 1));
+    };
+    Snake.prototype.Step = function () {
+        for (var i = this.Points.length - 1; i > 0; i--) {
+            this.Points[i].X = this.Points[i - 1].X;
+            this.Points[i].Y = this.Points[i - 1].Y;
+        }
+        switch (this.direction % 4) {
+            case 0:
+                this.Head().Y--;
+                break;
+            case 1:
+                this.Head().X++;
+                break;
+            case 2:
+                this.Head().Y++;
+                break;
+            case 3:
+                this.Head().X--;
+                break;
+            default:
+                throw new Error("Unknown direction");
+        }
+        if (this.Points[1]) {
+            if (this.Head().Compare(this.Points[1])) {
+                alert("You dead");
+            }
+        }
+    };
     return Snake;
 }());
 var Game = /** @class */ (function () {
@@ -26,26 +84,27 @@ var Game = /** @class */ (function () {
     }
     Game.prototype.Draw = function () {
         if (canv) {
-            var CellSize_1 = 20;
+            var cont = canv.getContext("2d");
+            cont.clearRect(0, 0, 10000, 10000);
             var _loop_1 = function (i) {
                 var _loop_2 = function (j) {
                     if (this_1.Apple.X == i && this_1.Apple.Y == j) {
-                        canv.getContext("2d").fillStyle = "red";
-                        canv.getContext("2d").fillRect(i * CellSize_1, j * CellSize_1, CellSize_1, CellSize_1);
+                        cont.fillStyle = "red";
+                        cont.fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
                     }
                     else {
                         found = false;
-                        this_1.snake.Points.forEach(function (point) {
+                        this_1.snake.Points.forEach(function (point, index) {
                             if (point.X == i && point.Y == j) {
-                                canv.getContext("2d").fillStyle = "green";
-                                canv.getContext("2d").fillRect(i * CellSize_1, j * CellSize_1, CellSize_1, CellSize_1);
+                                cont.fillStyle = index == 0 ? "lightgreen" : "green";
+                                cont.fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
                                 found = true;
                             }
                         });
                         if (found)
                             return "continue";
-                        canv.getContext("2d").fillStyle = "black";
-                        canv.getContext("2d").fillRect(i * CellSize_1, j * CellSize_1, CellSize_1, CellSize_1);
+                        cont.fillStyle = "black";
+                        cont.fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
                     }
                 };
                 for (var j = 0; j < this_1.Size; j++) {
@@ -60,8 +119,60 @@ var Game = /** @class */ (function () {
         else
             throw new Error("canvas is null");
     };
+    Game.prototype.NewApple = function () {
+        var p = Point.RandomPoint(this.Size);
+        while (true) {
+            p = Point.RandomPoint(this.Size);
+            var f = false;
+            this.snake.Points.forEach(function (element) {
+                if (element.Compare(p))
+                    f = true;
+            });
+            if (!f)
+                break;
+        }
+        this.Apple = p;
+    };
+    Game.prototype.Step = function (dir, redraw) {
+        this.snake.direction = dir;
+        this.snake.Step();
+        if (this.snake.Head().Compare(this.Apple)) {
+            this.snake.NewPoint();
+            this.NewApple();
+        }
+        if (redraw)
+            this.Draw();
+    };
     return Game;
 }());
-var game = new Game(30);
+var game = new Game(28);
 game.Draw();
-console.log(game);
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    switch (event.key) {
+        case "W":
+            game.Step(0, true);
+            break;
+        case "w":
+            game.Step(0, true);
+            break;
+        case "D":
+            game.Step(1, true);
+            break;
+        case "d":
+            game.Step(1, true);
+            break;
+        case "S":
+            game.Step(2, true);
+            break;
+        case "s":
+            game.Step(2, true);
+            break;
+        case "A":
+            game.Step(3, true);
+            break;
+        case "a":
+            game.Step(3, true);
+            break;
+    }
+});
--- Game.ts
+++ Game.ts
@@ -1,4 +1,5 @@
 const canv: HTMLCanvasElement | null = document.getElementById("game_canv") as HTMLCanvasElement;
+const CellSize:number = 18;
 class Point{
     public X: number;
     public Y: number;
@@ -9,11 +10,68 @@ class Point{
     static RandomPoint(max: number): Point{
         return new Point(Math.round(Math.random() * max), Math.round(Math.random() * max));
     }
+    public Compare(p: Point): boolean{
+        return p.X == this.X && p.Y == this.Y;
+    }
 }
 class Snake{
     public Points: Point[];
+    public direction: number = 0;
     constructor(StartPoint: Point){
         this.Points = new Array(StartPoint);
+    }
+    public Head() : Point{
+        return this.Points[0];
+    }
+    public Tall() : Point{
+        return this.Points[this.Points.length - 1];
+    }
+    public NewPoint(): void{
+        var tall: Point = this.Tall();
+        /*
+        if(this.Points.length < 2){
+            this.Points.push(new Point(tall.X, tall.Y + 1));
+            return;
+        }
+        if(this.Points[this.Points.length - 2].X == tall.X && this.Points[this.Points.length - 2].Y == tall.Y + 1){
+            this.Points.push(new Point(tall.X, tall.Y - 1));
+        }
+        else if(this.Points[this.Points.length - 2].X == tall.X + 1 && this.Points[this.Points.length - 2].Y == tall.Y){
+            this.Points.push(new Point(tall.X - 1, tall.Y));
+        }
+        else if(this.Points[this.Points.length - 2].X == tall.X - 1 && this.Points[this.Points.length - 2].Y == tall.Y){
+            this.Points.push(new Point(tall.X + 1, tall.Y));
+        }
+        else this.Points.push(new Point(tall.X, tall.Y + 1));
+        */
+        this.Points.push(new Point(tall.X, tall.Y + 1));
+    }
+    public Step(): void {
+        for (let i = this.Points.length - 1; i > 0; i--) {
+            this.Points[i].X = this.Points[i - 1].X;
+            this.Points[i].Y = this.Points[i - 1].Y;
+        }
+        switch (this.direction % 4) {
+            case 0:
+                this.Head().Y--;
+                break;
+            case 1:
+                this.Head().X++;
+                break;
+            case 2:
+                this.Head().Y++;
+                break;
+            case 3:
+                this.Head().X--;
+                break;
+            default:
+                throw new Error("Unknown direction");
+        }
+        if(this.Points[1]){
+            if(this.Head().Compare(this.Points[1])){
+                alert("You dead");
+            }
+        }
     }
 }
 class Game{
@@ -29,32 +87,81 @@ class Game{
     }
     public Draw(): void{
         if(canv){
-            const CellSize:number = 20;
+            var cont = canv.getContext("2d");
+            cont.clearRect(0, 0, 10000, 10000);
             for (let i = 0; i < this.Size; i++) {
                 for (let j = 0; j < this.Size; j++) {
                     if(this.Apple.X == i && this.Apple.Y == j){
-                        canv.getContext("2d").fillStyle = "red";
-                        canv.getContext("2d").fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
+                        cont.fillStyle = "red";
+                        cont.fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
                     }
                     else {
                         var found: boolean = false;
-                        this.snake.Points.forEach(point => {
+                        this.snake.Points.forEach((point, index) => {
                             if(point.X == i && point.Y == j){
-                                canv.getContext("2d").fillStyle = "green";
-                                canv.getContext("2d").fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
+                                cont.fillStyle = index == 0 ? "lightgreen" : "green";
+                                cont.fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
                                 found = true;
                             }
                         });
                         if(found) continue;
-                        canv.getContext("2d").fillStyle = "black";
-                        canv.getContext("2d").fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
+                        cont.fillStyle = "black";
+                        cont.fillRect(i * CellSize, j * CellSize, CellSize, CellSize);
                     }
                 }
             }
         }
         else throw new Error("canvas is null");
     }
+    public NewApple(){
+        var p: Point = Point.RandomPoint(this.Size - 1);
+        while (true) {
+            p = Point.RandomPoint(this.Size);
+            var f: boolean = false;
+            this.snake.Points.forEach(element => {
+                if(element.Compare(p)) f=true;
+            });
+            if(!f) break;
+        }
+        this.Apple = p;
+    }
+    public Step(dir: number, redraw: boolean): void{
+        this.snake.direction = dir;
+        this.snake.Step();
+        if(this.snake.Head().Compare(this.Apple)){
+            this.snake.NewPoint();
+            this.NewApple();
+        }
+        if(redraw) this.Draw();
+    }
 }
-var game: Game = new Game(30);
+var game: Game = new Game(28);
 game.Draw();
-console.log(game);
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "W":
+            game.Step(0, true);
+            break;
+        case "w":
+            game.Step(0, true);
+            break;
+        case "D":
+            game.Step(1, true);
+            break;
+        case "d":
+            game.Step(1, true);
+            break;
+        case "S":
+            game.Step(2, true);
+            break;
+        case "s":
+            game.Step(2, true);
+            break;
+        case "A":
+            game.Step(3, true);
+            break;
+        case "a":
+            game.Step(3, true);
+            break;
+    }
+});
